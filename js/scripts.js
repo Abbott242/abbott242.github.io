a7098231122d31e1723ba861b5e7cd2ea2a9864b
Brett Petch <brettpetch@icloud.com>
2020-04-09 03:29:11 -0400

    Cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
+/*
+    (C) 2020 Brett Petch
+    Last modified: 2020-04-09
+*/
 var teaProjects = {
     drawer: {
         title: "Drawer",
-        model: "",
+        model: "models/drawer.glb",
         images: [],
         text: "An opportunity was created after an obsolete radio was removed from my father’s Mooney M20M plane. The now-empty slot in the avionics bay was a hazard for debris getting dropped inside, and would normally be covered up with a simple plate. However in order to put the space to good use, I had the idea to create a small drawer which could be used to store small objects. Snacks, pens, phones, and wallets would have a convenient place to go - within reach, but out of the way in the compact cockpit. The first issue to solve was heating. During operation, the avionics bay would heat tremendously, beyond the operating temperature of most normal 3D-printing filaments. Therefore, I decided to use a nylon alloy filament, PolyMaker’s “CoPa”. Though this added expense and complications during printing, the operating temperature of 180C was more than enough to take the heat during use.\n"
     },
@@ -15,13 +19,13 @@ var teaProjects = {
     },
     monitorStand: {
         title: "Monitor Stand",
-        model: "",
+        model: "models/ms.glb",
         images: [],
         text: ""
     },
     opalNecklace: {
         title: "Opal Necklace",
-        model: "",
+        model: "models/on.glb",
         images: [],
         text: ""
     },
@@ -33,15 +37,29 @@ var teaProjects = {
     },
     faceShields: {
         title: "Face Shields",
-        images: [],
         model: "",
+        images: [],
         text: "The COVID-19 pandemic has been wreaking havoc on the world, particularly the healthcare system. Many essential medical devices are in short supply at the moment, and a large demand for replacements exists. While some items such as ventilators and masks require fairly complex facilities to produce, the flexible manufacturing provided by 3D printers can help to make simpler parts. Prusa Research designed a 3D-printable face shield which has seen somewhat widespread adoption when traditionally manufactured alternatives are no longer available. \n" +
             "\n" +
             "Therefore, when Inksmith LTD ran a donation drive in my area to collect parts for these face shields, I decided to try my hand at producing some. Using my Prusa MK3S and a Creality CR-10S 3D-printer over the course of a week, I spent most of my filament in constant printing. This was done using many different brands and plastics, including PLA, XT, and PETG. While each type required tweaking in order to optimize both print quality and speed, the result was enough parts for 60 functional face shields. Though certainly not ideal, once assembled and in the right hands they should provide a basic level of protection from the virus.\n"
     }
 };
 
+/*
+$(document).ready(function(){
+    for (item in teaProjects) {
+
+    }
+})
+ */
+
+ */
 $('#exampleModal').on('show.bs.modal', function (event) {
+    /*
+    # Modal Modify
+         The following function sets out to change the contents of the default modal. It uses jquery to find each item,
+         then goes through the dictionary to find relevant information.
+    */
     var button = $(event.relatedTarget); // Button that triggered the modal
     var projectID = button.data('whatever'); // Extract info from data-* attributes
     var project = teaProjects[projectID];
@@ -49,16 +67,66 @@ $('#exampleModal').on('show.bs.modal', function (event) {
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this)
     modal.find('.modal-title').text('About ' + teaProjects[projectID]['title'] + ' project');
-    for (i = 0; i < project['images'].length; i++) {
-        if (i === 0) {
-            modal.find('.modal-body .carousel-indicators').prepend('<li data-target="#modalCarousel" data-slide-to="'+ i +'" class="active"></li>')
-            modal.find('.modal-body .carousel-inner').prepend('<div class="carousel-item active"> <img class="d-block w-100" src="' + project['images'][i] + '" alt="carousel image"/> </div>');
+    if (project['images'].length > 0) {
+        modal.find('.carousel').removeClass('d-none');
+        for (i = 0; i < project['images'].length; i++) {
+            if (i === 0) {
+                modal.find('.modal-body .carousel-indicators').prepend('<li data-target="#modalCarousel" data-slide-to="' + i + '" class="active"></li>')
+                modal.find('.modal-body .carousel-inner').prepend('<div class="carousel-item active"> <img class="d-block w-100" src="' + project['images'][i] + '" alt="carousel image"/> </div>');
+            } else {
+                modal.find('.modal-body .carousel-indicators').prepend('<li data-target="#modalCarousel" data-slide-to="' + i + '"></li>')
+                modal.find('.modal-body .carousel-inner').prepend('<div class="carousel-item"> <img class="d-block w-100" src="' + project['images'][i] + '" alt="carousel image" /> </div>');
+            }
         }
-        else {
-            modal.find('.modal-body .carousel-indicators').prepend('<li data-target="#modalCarousel" data-slide-to="'+ i +'"></li>')
-            modal.find('.modal-body .carousel-inner').prepend('<div class="carousel-item"> <img class="d-block w-100" src="' + project['images'][i] + '" alt="carousel image" /> </div>');
-        }
+    } if (project['model'] || !project['images'].length) {
+        modal.find('.modal-body .col-5').prepend('<div class="card-background"><model-viewer class="card-img-top" src="'+ project['model'] + '" alt="' + project['title'] + '" auto-rotate camera-controls interaction-prompt="none"></model-viewer></div>')
     }
     modal.find('.modal-body .main-title').text(project['title']);
     modal.find('.modal-body .main-text').text(project['text'])
 });
+
+$("#exampleModal").on("hidden.bs.modal", function() {
+    /*
+    Modal Reset
+        The following function resets the contents of the modal window when it is hidden to get ready for the next open.
+        */
+    $(".modal-dialog").html('<div class="modal-content">\n' +
+        '            <div class="modal-header">\n' +
+        '                <h5 class="modal-title" id="exampleModalLabel">About</h5>\n' +
+        '                <button type="button" class="close" data-dismiss="modal" aria-label="Close">\n' +
+        '                    <span aria-hidden="true">&times;</span>\n' +
+        '                </button>\n' +
+        '            </div>\n' +
+        '            <div class="modal-body">\n' +
+        '                <div class="container-fluid">\n' +
+        '                    <div class="row">\n' +
+        '                        <div class="col-5">\n' +
+        '                            <div id="modalCarousel" class="carousel slide carousel-fade d-none" data-ride="carousel">\n' +
+        '                                <ol class="carousel-indicators">\n' +
+        '                                </ol>\n' +
+        '                                <div class="carousel-inner" role="listbox">\n' +
+        '                                </div>\n' +
+        '                                <a class="carousel-control-prev" href="#modalCarousel" role="button" data-slide="prev">\n' +
+        '                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>\n' +
+        '                                    <span class="sr-only">Previous</span>\n' +
+        '                                </a>\n' +
+        '                                <a class="carousel-control-next" href="#modalCarousel" role="button" data-slide="next">\n' +
+        '                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>\n' +
+        '                                    <span class="sr-only">Next</span>\n' +
+        '                                </a>\n' +
+        '                            </div>\n' +
+        '                        </div>\n' +
+        '                        <div class="col-7">\n' +
+        '                            <h3 class="main-title"></h3>\n' +
+        '                            <p class="main-text">\n' +
+        '                            </p>\n' +
+        '                        </div>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '            <div class="modal-footer">\n' +
+        '                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\n' +
+        '            </div>'
+    );
+});
+
